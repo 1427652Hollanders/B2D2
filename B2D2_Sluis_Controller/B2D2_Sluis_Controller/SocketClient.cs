7c035747df70a941fbe1e619264c77c7f0a221dd
1427652Hollanders <Wouter@DESKTOP-S311OJR>
2017-06-28 09:22:24 +0200

    Init sluis project - added socket & server client - Classes map added

---
+++ B2D2_Sluis_Controller/B2D2_Sluis_Controller/SocketClient.cs
@@ -0,0 +1,98 @@
+﻿/************************************************
+ * Author: Rianne Boumans                       *
+ * Zuyd Hogeschool; B2D2                        *
+ * Date: 01-2017                                *
+ *                                              *
+ * B2D2 Example project                         *                                                      
+ * WifiCommunication between 2 Raspberries      *
+ * Based on project from Dion, Wouter and Tim   *
+ ************************************************/
+
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Windows.Networking;
+using Windows.Networking.Sockets;
+using Windows.Storage.Streams;
+
+namespace B2D2_Sluis_Controller
+{
+    public class SocketClient
+    {
+        //IP adress van de Client die geconnect is
+        private readonly string _ip;
+        //Poort waar de client op luistert
+        private readonly int _port;
+        private StreamSocket _socket;
+        private DataWriter _writer;
+
+        public string Ip { get { return _ip; } }
+        public int Port { get { return _port; } }
+
+        /// <summary>
+        /// Initialiseer client en maak verbinding met de server
+        /// </summary>
+        /// <param name="ip"></param>
+        /// <param name="port"></param>
+        public SocketClient(string ip, int port)
+        {
+            _ip = ip;
+            _port = port;
+            Task.Run(() => Connect()).Wait();            
+        }
+
+        /// <summary>
+        /// Open een StreamSocket om data te kunnen versturen naar deze client.
+        /// </summary>
+        public async void Connect()
+        {            
+            try
+            {
+                var hostName = new HostName(Ip);
+                _socket = new StreamSocket();
+                await _socket.ConnectAsync(hostName, Port.ToString());
+                
+                _writer = new DataWriter(_socket.OutputStream);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Verstuur het bericht van de server naar deze client
+        /// </summary>
+        /// <param name="message">Bericht dat verstuurd moet worden</param>
+        public async void Verstuur(string message)
+        {
+            if (_writer != null)
+            {
+                _writer.WriteUInt32(_writer.MeasureString(message));
+                _writer.WriteString(message);
+
+                try
+                {
+                    await _writer.StoreAsync();
+                    await _writer.FlushAsync();
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine(ex.Message);
+                }
+            }
+            else
+            {
+                Debug.WriteLine("No Connection..");
+            }
+        }
+
+        public void Close()
+        {
+            _writer.DetachStream();
+            _writer.Dispose();
+
+            _socket.Dispose();
+        }
+    }
+}
---
+++ B2D2_Sluis_Controller/B2D2_Sluis_Controller/SocketServer.cs
@@ -0,0 +1,153 @@
+﻿/************************************************
+ * Author: Rianne Boumans                       *
+ * Zuyd Hogeschool; B2D2                        *
+ * Date: 01-2017                                *
+ *                                              *
+ * B2D2 Example project                         *                                                      
+ * WifiCommunication between 2 Raspberries      *
+ * Based on project from Dion, Wouter and Tim   *
+ ************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net;
+using System.Threading.Tasks;
+using Windows.Networking.Sockets;
+using Windows.Storage.Streams;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace B2D2_Sluis_Controller
+{
+    public class SocketServer
+    {
+        //Poortnummer waar de server op luistert (zie constructor)
+        private readonly int _port;
+        //Listner die luistert voor nieuwe data op de meegegeven poort        
+        private StreamSocketListener listener;
+
+        public delegate void DataOntvangenDelegate(string data);
+
+        public DataOntvangenDelegate OnDataOntvangen;
+
+        private List<SocketClient> teams;
+        private List<string> teamIP = new List<string>();
+
+        /// <summary>
+        /// Initialiseer en start de server
+        /// </summary>
+        /// <param name="port"></param>
+        public SocketServer(int port)
+        {
+            teams = new List<SocketClient>();
+            _port = port;
+            Start();
+        }
+
+        /// <summary>
+        /// Start de listner die luistert naar inkomende connecties
+        /// </summary>
+        public async void Start()
+        {
+            listener = new StreamSocketListener();
+            listener.ConnectionReceived += Listener_ConnectionReceived;
+
+            Debug.WriteLine("Wacht op connecties..");
+            await listener.BindServiceNameAsync(_port.ToString());
+        }
+
+        /// <summary>
+        /// Zodra een bericht binnen komt, wordt via de listner deze methode aangeroepen.
+        /// </summary>
+        /// <param name="bericht"></param>
+        public async void Server_OnDataOntvangen(string bericht)
+        {
+            IPAddress addr;
+            if (IPAddress.TryParse(bericht, out addr))
+            {
+                Debug.WriteLine("Bericht ontvangen van ip: " + bericht);
+                //Als hij niet in de lijst voorkomt, is het een nieuw team
+                Task.Delay(150).Wait();
+                if (!teamIP.Contains(bericht))
+                {
+                    teamIP.Add(bericht);
+                    SocketClient tmp = new SocketClient(bericht, 9000);
+                    teams.Add(tmp);
+                    VerstuurBerichtIedereen("Team " + teams.Count.ToString() + " doet mee!");
+
+                    Debug.WriteLine("Team " + teams.Count.ToString() + " doet mee!");
+                }
+                else
+                {
+                    Debug.WriteLine("IP adres heeft al connectie, niks mee doen");
+                }
+            }
+            else
+            {
+                Debug.WriteLine("Bericht ontvangen van een Team: " + bericht);
+
+
+                await SentMessageBack(bericht);
+            }
+        }
+
+        private async Task SentMessageBack(string bericht)
+        {
+            await Windows.ApplicationModel.Core.CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(
+                CoreDispatcherPriority.Normal,
+                () =>
+                {
+                    //code voor bericht ontvangen
+                }
+            );
+        }
+
+        /// <summary>
+        /// Zodra de listner een nieuwe connectie binnen krijgt, wordt deze methode aangeroepen
+        /// Deze zal het bericht controleren op compleetheid en daarna doorsturen naar de OnDataOntvangen methode.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        public async void Listener_ConnectionReceived(StreamSocketListener sender,
+            StreamSocketListenerConnectionReceivedEventArgs args)
+        {
+            var reader = new DataReader(args.Socket.InputStream);
+            try
+            {
+                while (true)
+                {
+                    uint sizeFieldCount = await reader.LoadAsync(sizeof(uint));
+                    if (sizeFieldCount != sizeof(uint)) return; //Disconnect
+                    uint stringLength = reader.ReadUInt32();
+                    uint actualStringLength = await reader.LoadAsync(stringLength);
+                    if (stringLength != actualStringLength) return; //Disconnect
+
+                    //Zodra data binnen is en er is een functie gekoppeld aan het event:                    
+                    if (OnDataOntvangen != null)
+                    {
+                        //Trigger het event, zodat er iets gedaan wordt met de ontvangen data
+                        OnDataOntvangen(reader.ReadString(actualStringLength));
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// In dit geval wordt hetzelfde bericht gestuurd naar iedere socket die aangemeld wordt bij de server
+        /// </summary>
+        /// <param name="bericht">Het te versturen bericht</param>
+        public void VerstuurBerichtIedereen(string bericht)
+        {
+            foreach (SocketClient sc in teams)
+            {
+                if (sc != null) sc.Verstuur(bericht);
+            }
+        }
+    }
+}
