bf428a7aed8454c13403d88d12e1bbb36eca372e
Nursize <Nursize@DESKTOP-SHFLSLO>
2017-07-02 19:36:29 +0200

    verkeerslicht aangepast

--- B2D2_Sluis_Controller/B2D2_Sluis_Controller/Classes/verkeerslicht.cs
+++ B2D2_Sluis_Controller/B2D2_Sluis_Controller/Classes/verkeerslicht.cs
@@ -8,22 +8,37 @@ namespace B2D2_Sluis_Controller.Classes
 {
     public class Verkeerslicht
     {
-        public int VerkeerslichtGroen { get; set; }
-        public int VerkeerslichtRood { get; set; }
-        public int VerkeerslichtRechts { get; set; }
-        public int VerkeerslichtLinks { get; set; }
+        private int TrafficLightGreen { get; set; }
+        private int TrafficLightRed { get; set; }
+        private int Gate1 { get; set; }
+        private int Gate2 { get; set; }
 
-        public Verkeerslicht(int rood, int groen, int rechts, int links)
+        private bool IsTrafficLightGreen = true;
+        private bool IsTrafficLightRed = false;
+
+        //public Verkeerslicht(int red, int green, int sluispoort1, int sluispoort2)
+        //{
+        //    TrafficLightGreen = green;
+        //    TrafficLightRed = red;
+        //    SluisPoort = sluispoort1;
+        //    SluisPoort = sluispoort2;
+        //}
+
+
+        public void ChangeColor(int GateSide, int TrafficLightColor, int red, int green)
         {
-            VerkeerslichtGroen = groen;
-            VerkeerslichtRood = rood;
-            VerkeerslichtRechts = rechts;
-            VerkeerslichtLinks = links;
+            if (IsTrafficLightRed)
+            {
+                GateSide = Gate1;
+                GateSide = Gate2;
+                TrafficLightColor = red;
+            }
+            else if (IsTrafficLightGreen)
+            {
+                GateSide = Gate1;
+                GateSide = Gate2;
+                TrafficLightColor = green;
+            }
         }
-        private bool IsVerkeerslichtGroen = true;
-        private bool IsVerkeerslichtRood = false;
     }
-    // private void WachtEenVaartuig 
-    // {
-    //     if (IsVerkeerslichtGroen) 
 }
